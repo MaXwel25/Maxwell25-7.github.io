fef47411419b1be589093a013bd815ea06191b21
MaXwel25 <167020012+MaXwel25@users.noreply.github.com>
2024-12-05 16:19:24 +0300

    Add files via upload

--- orderform.js
+++ orderform.js
@@ -104,3 +104,20 @@ window.addEventListener('DOMContentLoaded', function (event) {
 
   updatePrice();
 });
+
+$(document).ready(function () {
+  $(".gallery").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".gallery").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+
